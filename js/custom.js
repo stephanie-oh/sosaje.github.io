9c234e4dbda84392718c09c25df7b14657306f52
SoOsaje <54948120+SoOsaje@users.noreply.github.com>
2021-11-01 22:08:07 +0100

    extras

--- js/custom.js
+++ js/custom.js
@@ -155,7 +155,7 @@
         if ($('.fullpage-default').length) {
             var myFullpage = new fullpage('.fullpage-default', {
                 licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
-                anchors: ['home', 'about', 'services', 'skills', 'work', 'contact', 'slide07'],
+                anchors: ['home', 'about', 'work', 'contact',],
                 menu: '#nav',
                 lazyLoad: true,
                 navigation: true,
